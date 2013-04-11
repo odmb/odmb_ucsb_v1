1fdd593128112135a7f8e238341c20720e94e76f
manuelf <>
2013-04-11 16:28:38 +0000

    According to Guido, cafifo now works

--- source/odmb_ctrl/cafifo.vhd
+++ source/odmb_ctrl/cafifo.vhd
@@ -15,6 +15,10 @@ library IEEE;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 --use IEEE.STD_LOGIC_INTEGER.all;
 use IEEE.STD_LOGIC_1164.all;
+LIBRARY UNISIM;
+USE UNISIM.vcomponents.all;
+LIBRARY UNIMACRO;
+USE UNIMACRO.vcomponents.all;
 
 entity cafifo is
    generic (
@@ -85,6 +89,10 @@ type l1a_array_type is array (FIFO_SIZE-1 downto 0) of std_logic_vector(NFEB+2 d
 signal l1a_match : l1a_array_type;
 signal l1a_dav : l1a_array_type;
 
+type l1a_b_array_type is array (FIFO_SIZE-1 downto 0) of std_logic;
+signal l1a_dav_b8, l1a_dav_b8_gm : l1a_b_array_type;
+signal l1a_dav_b9, l1a_dav_b9_gm : l1a_b_array_type;
+
 signal l1a_cnt_wren, l1a_match_wren : std_logic;
 signal wr_addr_en, rd_addr_en : std_logic;
 -- signal wr_addr_out, rd_addr_out : std_logic_vector(3 downto 0);
@@ -92,6 +100,19 @@ signal wr_addr_out, rd_addr_out : integer;
 
 signal wren, rden : std_logic;
 signal empty, full : std_logic;
+
+signal reg_alct_l1a_dav : std_logic;
+signal alct_fifo_empty, alct_fifo_full : std_logic;
+signal alct_fifo_wr_en, alct_fifo_rd_en : std_logic;
+signal alct_fifo_wr_cnt, alct_fifo_rd_cnt : std_logic_vector(9 downto 0);
+signal alct_fifo_in, alct_fifo_out : std_logic_vector(15 downto 0);
+
+signal reg_tmb_l1a_dav : std_logic;
+signal tmb_fifo_empty, tmb_fifo_full : std_logic;
+signal tmb_fifo_wr_en, tmb_fifo_rd_en : std_logic;
+signal tmb_fifo_wr_cnt, tmb_fifo_rd_cnt : std_logic_vector(9 downto 0);
+signal tmb_fifo_in, tmb_fifo_out : std_logic_vector(15 downto 0);
+
     
 begin
 
@@ -323,27 +344,139 @@ begin
 	end if;
 
 end process;
+
+alct_reg : process (alct_l1a_dav, rst, clk)
+
+begin
+	if (rst = '1') then
+	   reg_alct_l1a_dav <= '0';
+	elsif rising_edge(clk) then
+	   reg_alct_l1a_dav <= alct_l1a_dav;
+	end if;
+	
+end process;
+
+alct_fifo_wr_en <= l1a_match_in(9);
+alct_fifo_rd_en <= alct_l1a_dav;
+alct_fifo_in <= "0000" & l1a_cnt_out;
+  
+alct_fifo : FIFO_SYNC_MACRO
+   generic map (
+      DEVICE => "VIRTEX6",            -- Target Device: "VIRTEX5, "VIRTEX6" 
+      ALMOST_FULL_OFFSET => X"0080",  -- Sets almost full threshold
+      ALMOST_EMPTY_OFFSET => X"0080", -- Sets the almost empty threshold
+      DATA_WIDTH => 16,   -- Valid values are 1-72 (37-72 only valid when FIFO_SIZE="36Kb")
+      FIFO_SIZE => "18Kb")            -- Target BRAM, "18Kb" or "36Kb" 
+   port map (
+      ALMOSTEMPTY => open,   -- Output almost empty 
+      ALMOSTFULL => open,     -- Output almost full
+      DO => alct_fifo_out,                     -- Output data
+      EMPTY => alct_fifo_empty,               -- Output empty
+      FULL => alct_fifo_full,                 -- Output full
+      RDCOUNT => alct_fifo_rd_cnt,           -- Output read count
+      RDERR => open,               -- Output read error
+      WRCOUNT => alct_fifo_wr_cnt,           -- Output write count
+      WRERR => open,               -- Output write error
+      CLK => clk,                   -- Input clock
+      DI => alct_fifo_in,                     -- Input data
+      RDEN => alct_fifo_rd_en,                 -- Input read enable
+      RST => rst,                   -- Input reset
+      WREN => alct_fifo_wr_en                  -- Input write enable
+   );
+
+alct_dv_fifo_gm : process (l1a_cnt, alct_fifo_out, reg_alct_l1a_dav, rst, clk)
+
+begin
+	if (rst = '1') then
+	  for index in 0 to FIFO_SIZE-1 loop
+      l1a_dav_b9_gm(index) <= '0';
+	  end loop;
+	elsif rising_edge(clk) then
+	  for index in 0 to FIFO_SIZE-1 loop      
+	    if (alct_fifo_out(11 downto 0) = l1a_cnt(index)) and (reg_alct_l1a_dav = '1') then
+         l1a_dav_b9_gm(index) <= '1';
+      end if;
+	  end loop;
+	end if;
+
+end process;
 	
 alct_dv_fifo : process (l1a_cnt, dcfeb_l1a_cnt, alct_l1a_dav, rst, clk)
 variable filled : std_logic;
 begin
   filled := '0';
 	if (rst = '1') then
---	  for index in 0 to FIFO_SIZE-1 loop
---      dcfeb_l1a_dav(index) <= (OTHERS => '0');
---	  end loop;
+	  for index in 0 to FIFO_SIZE-1 loop
+      l1a_dav_b9(index) <= '0';
+	  end loop;
 	elsif rising_edge(clk) and alct_l1a_dav='1' then
 	  for index in wr_addr_out+1 to FIFO_SIZE-1 loop      
 	       if (l1a_match(index)(9) = '1') and (filled = '0') then
-            l1a_dav(index)(9) <= '1';
+            l1a_dav_b9(index) <= '1';
             filled := '1';
          end if;
 	  end loop;
 	  for index in 1 to wr_addr_out-1 loop      
 	       if (l1a_match(index)(9) = '1') and (filled = '0') then
-            l1a_dav(index)(9) <= '1';
+            l1a_dav_b9(index) <= '1';
             filled := '1';
          end if;
+	  end loop;
+	end if;
+
+end process;
+	
+tmb_reg : process (tmb_l1a_dav, rst, clk)
+
+begin
+	if (rst = '1') then
+	   reg_tmb_l1a_dav <= '0';
+	elsif rising_edge(clk) then
+	   reg_tmb_l1a_dav <= tmb_l1a_dav;
+	end if;
+	
+end process;
+
+tmb_fifo_wr_en <= l1a_match_in(8);
+tmb_fifo_rd_en <= tmb_l1a_dav;
+tmb_fifo_in <= "0000" & l1a_cnt_out;
+  
+tmb_fifo : FIFO_SYNC_MACRO
+   generic map (
+      DEVICE => "VIRTEX6",            -- Target Device: "VIRTEX5, "VIRTEX6" 
+      ALMOST_FULL_OFFSET => X"0080",  -- Sets almost full threshold
+      ALMOST_EMPTY_OFFSET => X"0080", -- Sets the almost empty threshold
+      DATA_WIDTH => 16,   -- Valid values are 1-72 (37-72 only valid when FIFO_SIZE="36Kb")
+      FIFO_SIZE => "18Kb")            -- Target BRAM, "18Kb" or "36Kb" 
+   port map (
+      ALMOSTEMPTY => open,   -- Output almost empty 
+      ALMOSTFULL => open,     -- Output almost full
+      DO => tmb_fifo_out,                     -- Output data
+      EMPTY => tmb_fifo_empty,               -- Output empty
+      FULL => tmb_fifo_full,                 -- Output full
+      RDCOUNT => tmb_fifo_rd_cnt,           -- Output read count
+      RDERR => open,               -- Output read error
+      WRCOUNT => tmb_fifo_wr_cnt,           -- Output write count
+      WRERR => open,               -- Output write error
+      CLK => clk,                   -- Input clock
+      DI => tmb_fifo_in,                     -- Input data
+      RDEN => tmb_fifo_rd_en,                 -- Input read enable
+      RST => rst,                   -- Input reset
+      WREN => tmb_fifo_wr_en                  -- Input write enable
+   );
+
+tmb_dv_fifo_gm : process (l1a_cnt, tmb_fifo_out, reg_tmb_l1a_dav, rst, clk)
+
+begin
+	if (rst = '1') then
+	  for index in 0 to FIFO_SIZE-1 loop
+      l1a_dav_b8_gm(index) <= '0';
+	  end loop;
+	elsif rising_edge(clk) then
+	  for index in 0 to FIFO_SIZE-1 loop      
+	    if (tmb_fifo_out(11 downto 0) = l1a_cnt(index)) and (reg_tmb_l1a_dav = '1') then
+         l1a_dav_b8_gm(index) <= '1';
+      end if;
 	  end loop;
 	end if;
 
@@ -354,19 +487,19 @@ variable filled : std_logic;
 begin
   filled := '0';
 	if (rst = '1') then
---	  for index in 0 to FIFO_SIZE-1 loop
---      dcfeb_l1a_dav(index) <= (OTHERS => '0');
---	  end loop;
+	  for index in 0 to FIFO_SIZE-1 loop
+      l1a_dav_b8(index) <= '0';
+	  end loop;
 	elsif rising_edge(clk) and tmb_l1a_dav='1' then
 	  for index in wr_addr_out+1 to FIFO_SIZE-1 loop      
 	       if (l1a_match(index)(8) = '1') and (filled = '0') then
-            l1a_dav(index)(8) <= '1';
+            l1a_dav_b8(index) <= '1';
             filled := '1';
          end if;
 	  end loop;
 	  for index in 1 to wr_addr_out-1 loop      
 	       if (l1a_match(index)(8) = '1') and (filled = '0') then
-            l1a_dav(index)(8) <= '1';
+            l1a_dav_b8(index) <= '1';
             filled := '1';
          end if;
 	  end loop;
